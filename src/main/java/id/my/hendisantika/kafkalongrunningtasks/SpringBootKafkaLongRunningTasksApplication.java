9f813d73eb6e8dbfed1f70b395d92fff660b8eb6
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 08:49:16 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkalongrunningtasks/SpringBootKafkaLongRunningTasksApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kafkalongrunningtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaLongRunningTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaLongRunningTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/kafkalongrunningtasks/SpringBootKafkaLongRunningTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kafkalongrunningtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaLongRunningTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
