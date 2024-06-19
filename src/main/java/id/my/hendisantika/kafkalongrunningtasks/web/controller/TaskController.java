872ddfdcdfb26f09c3ea204efdcee3af2d74de75
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 09:03:20 +0700

    Add processAsync in TaskController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkalongrunningtasks/web/controller/TaskController.java
+++ src/main/java/id/my/hendisantika/kafkalongrunningtasks/web/controller/TaskController.java
@@ -1,11 +1,20 @@
 package id.my.hendisantika.kafkalongrunningtasks.web.controller;
 
 import id.my.hendisantika.kafkalongrunningtasks.jms.service.KafkaConsumerService;
+import id.my.hendisantika.kafkalongrunningtasks.web.model.TaskRequest;
+import id.my.hendisantika.kafkalongrunningtasks.web.model.TaskResponse;
 import id.my.hendisantika.kafkalongrunningtasks.web.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +35,12 @@ public class TaskController {
 
     private final KafkaConsumerService kafkaConsumerService;
 
+    @PostMapping
+    public ResponseEntity<TaskResponse> processAsync(@RequestBody TaskRequest taskRequest,
+                                                     UriComponentsBuilder b) {
+        String taskId = UUID.randomUUID().toString();
+        UriComponents progressURL = b.path("/tasks/{id}/progress").buildAndExpand(taskId);
+        taskService.process(taskId, taskRequest, b);
+        return ResponseEntity.accepted().location(progressURL.toUri()).build();
+    }
 }
