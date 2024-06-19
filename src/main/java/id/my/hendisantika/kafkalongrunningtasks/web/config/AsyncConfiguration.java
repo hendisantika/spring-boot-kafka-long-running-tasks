ddcbf29eacfeb5cabe49f8d23f5fcdcd4acb5152
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 08:57:52 +0700

    Add AsyncConfiguration🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkalongrunningtasks/web/config/AsyncConfiguration.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.kafkalongrunningtasks.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-long-running-tasks
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/19/24
+ * Time: 08:57
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+        return executor;
+    }
+}
