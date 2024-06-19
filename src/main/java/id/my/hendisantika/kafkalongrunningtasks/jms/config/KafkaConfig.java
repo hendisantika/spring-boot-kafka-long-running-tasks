a21f93aa218e7e3f24f4e684ea0f9c051efccd4c
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 08:52:53 +0700

    Add consumerFactory in KafkaConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkalongrunningtasks/jms/config/KafkaConfig.java
+++ src/main/java/id/my/hendisantika/kafkalongrunningtasks/jms/config/KafkaConfig.java
@@ -2,12 +2,19 @@ package id.my.hendisantika.kafkalongrunningtasks.jms.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,5 +45,18 @@ public class KafkaConfig {
         KafkaAdmin kafkaAdmin = new KafkaAdmin(kafkaProperties.buildAdminProperties());
         kafkaAdmin.setFatalIfBrokerNotAvailable(kafkaProperties.getAdmin().isFailFast());
         return kafkaAdmin;
+    }
+
+    @Bean
+    public ConsumerFactory<String, TaskStatus> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "task-group");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
     }
 }
