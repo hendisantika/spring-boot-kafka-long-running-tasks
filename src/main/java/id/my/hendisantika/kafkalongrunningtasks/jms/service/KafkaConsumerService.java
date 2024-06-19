cca6e62726f805b1df1394c466685d8b27e501c3
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 08:57:12 +0700

    Add getLatestTaskStatus in KafkaConsumerService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkalongrunningtasks/jms/service/KafkaConsumerService.java
+++ src/main/java/id/my/hendisantika/kafkalongrunningtasks/jms/service/KafkaConsumerService.java
@@ -3,8 +3,13 @@ package id.my.hendisantika.kafkalongrunningtasks.jms.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Iterator;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +28,17 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumerService {
 
     private final KafkaConsumer<String, TaskStatus> kafkaConsumer;
+
+    public TaskStatus getLatestTaskStatus(String taskId) {
+        ConsumerRecord<String, TaskStatus> latestUpdate = null;
+        ConsumerRecords<String, TaskStatus> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+        if (!consumerRecords.isEmpty()) {
+            Iterator itr = consumerRecords.records(taskId).iterator();
+            while (itr.hasNext()) {
+                latestUpdate = (ConsumerRecord<String, TaskStatus>) itr.next();
+            }
+            log.info("Latest updated status : " + latestUpdate.value());
+        }
+        return latestUpdate != null ? latestUpdate.value() : null;
+    }
 }
